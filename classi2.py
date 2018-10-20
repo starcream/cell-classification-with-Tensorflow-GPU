cb133fa90ad04cd8b0ed7d14d54c3b2783f0121e
King Yizhi <13121227218@163.com>
2018-10-20 20:29:25 +0800

    Update classi2.py

--- classi2.py
+++ classi2.py
@@ -1,4 +1,4 @@
-from skimage import io,transform
+from skimage import io, transform
 import glob
 import os
 import tensorflow as tf
@@ -6,7 +6,7 @@ import numpy as np
 
 # training set : 8701 ,validation set : 2175 , six kinds of cells
 train_path = 'D:\\DL_data\\training\\'
-val_path   = 'D:\\DL_data\\validation\\'
+val_path = 'D:\\DL_data\\validation\\'
 # each image : 78 * 78 * 1
 w = 78
 h = 78
@@ -14,28 +14,30 @@ c = 1
 n_epoch = 130
 batch_size = 400
 
-# function to read image ,return imgs and labels
+
+# function to read image ,shuffle,return imgs and labels
 def read_img(path):
     category = [path + x for x in os.listdir(path) if os.path.isdir(path + x)]
     imgs = []
     labels = []
     for idx, folder in enumerate(category):
-        print(idx,'   ',folder)
+        print(idx, '   ', folder)
         # get images and labels in certain dir
         for im in glob.glob(folder + '\\*.png'):
             img = io.imread(im)
             img = transform.resize(img, (w, h, c))
             imgs.append(img)
             labels.append(idx)
-        data = np.asarray(imgs,np.float32)
-        labels = np.asarray(labels,np.int32)
-        # shuffle images and label
-        num = data.shape[0]
-        arr = np.arange(num)
-        np.random.shuffle(arr)
-        data = data[arr]
-        labels = labels[arr]
-    return data,labels
+    data = np.asarray(imgs, np.float32)
+    labels = np.asarray(labels, np.int32)
+    # shuffle images and label
+    num = data.shape[0]
+    arr = np.arange(num)
+    np.random.shuffle(arr)
+    data = data[arr]
+    labels = labels[arr]
+    return data, labels
+
 
 # get batch of data
 def getbatch(inputs=None, targets=None, batch_size=None, shuffle=False):
@@ -50,16 +52,18 @@ def getbatch(inputs=None, targets=None, batch_size=None, shuffle=False):
             excerpt = slice(start_idx, start_idx + batch_size)
         yield inputs[excerpt], targets[excerpt]
 
+
 # read data
 print("read training set")
-x_train,y_train = read_img(train_path)
+x_train, y_train = read_img(train_path)
 print("reading validation set")
-x_val,y_val = read_img(val_path)
+x_val, y_val = read_img(val_path)
+
 # -----------------build network----------------------
 x = tf.placeholder(tf.float32, shape=[None, w, h, c], name='x')
 y_ = tf.placeholder(tf.int32, shape=[None, ], name='y_')
 
-# conv 1& pool1（78——>39)
+# conv 1& pool1（78*78*3 -> 78*78*32)
 conv1 = tf.layers.conv2d(
     inputs=x,
     filters=32,
@@ -67,12 +71,13 @@ conv1 = tf.layers.conv2d(
     padding="same",
     activation=tf.nn.relu,
     kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
+# 78*78*32 -》 39*39*32
 pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
 # conv2 & pool2 (39->13)
 conv2 = tf.layers.conv2d(
     inputs=pool1,
-    filters=64,
+    filters=16,
     kernel_size=[5, 5],
     padding="same",
     activation=tf.nn.relu,
@@ -81,18 +86,20 @@ pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=3)
 
 # conv3 & pool3 (13->5)
 conv3 = tf.layers.conv2d(
-    inputs =pool2,
-    filters = 128,
-    kernel_size = [4 , 4],
-    padding = "valid",
-    activation = tf.nn.relu,
-    kernel_initializer = tf.truncated_normal_initializer(stddev = 0.01)
+    inputs=pool2,
+    filters=128,
+    kernel_size=[4, 4],
+    padding="valid",
+    activation=tf.nn.relu,
+    kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)
 )
-pool3 = tf.layers.max_pooling2d(inputs = conv3,pool_size = [2,2],strides = 2)
+pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
 
+# dropout layer
 keep_prob = tf.placeholder(tf.float32)
 drop = tf.nn.dropout(pool3, keep_prob)
-re1 = tf.reshape(drop, [-1, 13 * 13 * 64])
+
+re1 = tf.reshape(drop, [-1, 5 * 5 * 128])
 
 # fcn , l2 reg
 dense1 = tf.layers.dense(inputs=re1,
@@ -113,7 +120,7 @@ logits = tf.layers.dense(inputs=dense2,
 # ---------------------------end of network---------------------------
 
 loss = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=logits)
-train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+train_op = tf.train.AdamOptimizer(learning_rate=0.001,beta1=0.9, beta2=0.999).minimize(loss)
 correct_prediction = tf.equal(tf.cast(tf.argmax(logits, 1), tf.int32), y_)
 acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -122,7 +129,7 @@ acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 for epoch in range(n_epoch):
-    print("epoch : ",epoch)
+    print("epoch : ", epoch)
     # training
     train_loss, train_acc, n_batch = 0, 0, 0
     for x_train_a, y_train_a in getbatch(x_train, y_train, batch_size, shuffle=True):
@@ -132,20 +139,24 @@ for epoch in range(n_epoch):
         train_loss += err
         train_acc += ac
         n_batch += 1
-    #print("   train loss: %f" % (train_loss / n_batch))
+    # print("   train loss: %f" % (train_loss / n_batch))
     print("   train acc: %f" % (train_acc / n_batch))
 
     # validation
-    if(epoch%20 == 19):
+    if (epoch % 20 == 19):
         val_loss, val_acc, n_batch = 0, 0, 0
         for x_val_a, y_val_a in getbatch(x_val, y_val, batch_size, shuffle=False):
             err, ac = sess.run([loss, acc], feed_dict={x: x_val_a,
                                                        y_: y_val_a,
-                                                       keep_prob:1.0})
+                                                       keep_prob: 1.0})
             val_loss += err
             val_acc += ac
             n_batch += 1
-        #print("   validation loss: %f" % (val_loss / n_batch))
+        # print("   validation loss: %f" % (val_loss / n_batch))
         print("   validation acc: %f" % (val_acc / n_batch))
 
 sess.close()
+
+# how to save model and read from model
+# how to adjust learning rate while training
+# how to achieve data augmentation
